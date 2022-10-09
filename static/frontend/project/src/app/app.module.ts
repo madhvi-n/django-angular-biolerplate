614ab558c0be5124195d7e883516d5ff18ad5cc0
madhvi-n <madhvinandanwar@gmail.com>
2022-10-09 19:46:45 +0530

    Frontend

--- static/frontend/project/src/app/app.module.ts
+++ static/frontend/project/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
